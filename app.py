c4c0ab738967f050d5189018817b331567dbf6cb
LaktionovVladyslav <lakworkmail@gmail.com>
2019-05-26 19:17:30 +0300

    Create App
    Create new repository and share to GitHub

---
+++ app.py
@@ -0,0 +1,90 @@
+from flask import Flask, render_template, request
+from flask_ipinfo import IPInfo
+from config import Config
+from constant import events
+import datetime
+import forms
+import pytz
+import requests
+
+time_zone = pytz.timezone('Etc/GMT+5')
+
+app: Flask = Flask(__name__)
+app.config.from_object(Config)
+ipinfo = IPInfo()
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html")
+
+
+def send_user_info():
+    data = {
+        "chat_id": "@helpex_channel",
+        "text": f"Browser: {ipinfo.browser}\nЯзык: {ipinfo.lang}\nОС: {ipinfo.os}\nIP: {ipinfo.ipaddress}\n{ipinfo.get_info}"
+    }
+    url = "https://api.telegram.org/bot598340268:AAG5LAPOcWOqEUXcSPjfXBJ4CXDY0hHDR3Y/sendMessage?"
+    resp = requests.get(url=url, params=data)
+    return resp
+
+
+@app.route('/check', methods=['GET', 'POST'])
+def check():
+    text = ''.join([f"{str(event)}  ==   {events[event]}<br><br><br><br>" for event in events.keys()])
+    return text
+
+
+@app.route('/', methods=['GET', 'POST'])
+def crib_form():
+    send_user_info()
+    data_form = forms.CribForm(request.form)
+    data_time = datetime.datetime.now(pytz.timezone('Europe/Kiev')).strftime("%d-%m-%Y %H:%M:%S")
+    if request.method == 'POST' and data_form.is_submitted():
+        input_data = {
+            "first_name": data_form.first_name.data,
+            "last_name": data_form.last_name.data,
+            "middle_name": data_form.middle_name.data,
+            "date_birthday": data_form.date_birthday.data,
+            "document": data_form.document.data,
+            "reg_place": data_form.reg_place.data,
+            "date_time_event": data_form.date_time_event.data,
+            "place_event": data_form.place_event.data,
+            "vehicle": data_form.vehicle.data,
+            "incident": data_form.incident.data,
+            "witness": data_form.witness.data,
+            "fine": data_form.fine.data,
+            "police_bio": data_form.police_bio.data,
+            "police_rang": data_form.police_rang.data,
+            "police_position": data_form.police_position.data,
+        }
+        print(input_data)
+        return render_template("crib/index.html", input_data=input_data)
+
+    return render_template("crib_form/index.html", data_form=data_form, data_time=data_time)
+
+
+@app.route('/crib')
+def crib():
+    input_data = {
+        "first_name": "first_name",
+        "last_name": "last_name",
+        "middle_name": "middle_name",
+        "date_birthday": "date_birthday",
+        "document": "document",
+        "reg_place": "reg_place",
+        "date_time_event": "date_time_event",
+        "place_event": "place_event",
+        "vehicle": "vehicle",
+        "incident": "incident",
+        "witness": "witness",
+        "fine": "fine",
+        "police_bio": "police_bio",
+        "police_rang": "police_rang",
+        "police_position": "police_position"
+    }
+    return render_template("crib/index.html", input_data=input_data)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ forms.py
@@ -0,0 +1,39 @@
+from flask_wtf import FlaskForm
+from constant import events
+from wtforms import StringField, SelectField, SubmitField, HiddenField
+from wtforms.validators import DataRequired, Length
+from wtforms.fields.html5 import DateField, DateTimeField
+
+ranges = ["Полковник поліції",
+          "Підполковник поліції",
+          "Майор поліції",
+          "Капітан поліції",
+          "Старший лейтенант поліції",
+          "Лейтенант поліції",
+          "Молодший лейтенант поліції",
+          "Старший сержант поліції",
+          "Сержант поліції",
+          "Капрал поліції",
+          "Рядовий поліції"]
+ranges.reverse()
+
+
+class CribForm(FlaskForm):
+    first_name = StringField("Ім\'я", validators=[DataRequired()])
+    last_name = StringField("Прізвище", validators=[DataRequired()])
+    middle_name = StringField("По батькові", validators=[DataRequired()])
+    date_birthday = DateField("Дата народження")
+    document = StringField("Документ", validators=[DataRequired()])
+    reg_place = StringField("Зареєстрований", validators=[DataRequired()])
+    date_time_event = StringField("Дата(час) розгляду", validators=[DataRequired()])
+    place_event = StringField("Місце розгляду", validators=[DataRequired()])
+    vehicle = StringField("Транспортний засіб", validators=[DataRequired()])
+    incident = SelectField("Порушення ПДР", validators=[DataRequired()],
+                           choices=[("", "Порушення"), *[(events[event], event) for event in events.keys()]])
+    witness = StringField("ПІБ свідка", validators=[DataRequired()])
+    fine = StringField("Cтягнення", validators=[DataRequired()])
+    police_bio = StringField("ПІБ", validators=[DataRequired()])
+    police_rang = SelectField("Спеціальне звання", choices=[(rang, rang) for rang in ranges],
+                              validators=[DataRequired()])
+    police_position = StringField("Посада", validators=[DataRequired()])
+    submit = SubmitField("Переглянути")
